cbad4b800dda030afa1a5ad52cc8af5a6f1b98e6
August Johnson <gurojohnson@gmail.com>
2026-02-01 19:31:07 -0500

    Write code to reverse strings

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArr = string.split('');
+    const reverseString = stringArr.reverse();
+    let backwardsString = reverseString.join('');
+    return backwardsString;
 };
 
 // Do not edit below this line
