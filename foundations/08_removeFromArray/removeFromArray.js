6ee01eb1cc4e5d24b58bed7f6c8d12c65fddbf45
August Johnson <gurojohnson@gmail.com>
2026-02-01 20:18:22 -0500

    Write function to remove given arg from given arr

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toBeRemoved) {
+    const newArray = [];
+    array.forEach((item) => {
+        if (!toBeRemoved.includes(item)) {
+            newArray.push(item);
+        }
+    }
+    )
+    return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
