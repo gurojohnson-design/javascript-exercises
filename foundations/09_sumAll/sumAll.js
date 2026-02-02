600fdc7556f0bb20b425c31b1c4d39bb5e377646
August Johnson <gurojohnson@gmail.com>
2026-02-02 08:18:04 -0500

    Write code to add all numbers within given range Function adds range of positive numbers including ends or returns error

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (Number.isInteger(a) && Number.isInteger(b) && a >= 0 && b >= 0) {
+        let sum = 0;
+        if (a < b) {
+            for (i = a; i <= b; i++) {
+                sum += i;
+            }
+        } else if (b < a) {
+            for (i = b; i <= a; i++) {
+                sum += i;
+            }
+        } return sum;
+    } else return 'ERROR'
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
