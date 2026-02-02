afc4edcbc9d89e6d581396d92a63256da505a661
August Johnson <gurojohnson@gmail.com>
2026-02-02 13:31:37 -0500

    Write function for C <=> F conversion rounded

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let toCelcius = (farenheit - 32) * (5/9);
+  if (toCelcius == 0) {
+    return 0;
+  } else {
+  let roundedCelcius = toCelcius.toFixed(1);
+  return Number(roundedCelcius);}
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let toFarenheit = (celcius * (9/5)) + 32;
+  if (toFarenheit == 0) {
+    return 0;
+  } else {
+  let roundedFarenheit = toFarenheit.toFixed(1);
+  return Number(roundedFarenheit);}
 };
+
 
 // Do not edit below this line
 module.exports = {
