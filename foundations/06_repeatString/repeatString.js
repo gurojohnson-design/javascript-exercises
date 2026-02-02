05514c8973e54780e30b80109dabeda3c02b1068
August Johnson <gurojohnson@gmail.com>
2026-02-01 19:19:20 -0500

    Define a function to repeate strings given '' and rep #

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repString = '';
+    if (num >= 0) {
+        
+        for (i = 1; i <= num; i++) {
+            repString += string;
+        }
+    } else return 'ERROR';
+return repString;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
