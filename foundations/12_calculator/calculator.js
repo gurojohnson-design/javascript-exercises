67b36080efac4c3dc3008782e0a9d2a94f2b31ef
August Johnson <gurojohnson@gmail.com>
2026-02-05 15:16:45 -0500

    Write 'calculator' functions wrote functions for addition of multiple numbers in array, subtraction, multiplication, division, to the power of, and factorial

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+  let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let difference = a - b;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const sumAll = arr.reduce((total, currentValue) => total + currentValue, 0);
+  return sumAll;
+  };
+
+const multiply = function(arr) {
+  const multiplyAll = arr.reduce((total, currentvalue) => total * currentvalue, 1);
+  return multiplyAll;
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	let power = a ** b;
+  return power;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0) {
+    alert('This number is imaginary');
+  } else if (num == 0 || num == 1) {
+    return 1;
+  } else {
+    let result = 1;
+	for (let i = 2; i <= num; i++) {
+    result *= i;
+  } return result;
+  }
 };
 
 // Do not edit below this line
