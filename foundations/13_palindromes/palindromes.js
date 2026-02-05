db30b3bde5f6163f591be33a68662721058590a6
August Johnson <gurojohnson@gmail.com>
2026-02-05 16:03:19 -0500

    Write function to determine if string = palindrome

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const strippedString = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedSting = strippedString.split('').reverse().join('');
+    return strippedString === reversedSting;
 };
+
+
+
+
+
+//make sure its a string
+//strip it to its letters
+//reverse it
+//compare that their equal
+//reutrn true or false
+
 
 // Do not edit below this line
 module.exports = palindromes;
