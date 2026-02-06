72e10587411530f7f5d2ad31f1a070eaad988fdc
August Johnson <gurojohnson@gmail.com>
2026-02-06 08:44:28 -0500

    Write function to get book titles from array

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    const titles = arr.map(book => book.title);
+    return titles;
 };
+
+
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
