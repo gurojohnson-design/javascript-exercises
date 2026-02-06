a9d701737925f309b3403010ee1ddc10175e3b4c
August Johnson <gurojohnson@gmail.com>
2026-02-06 10:30:50 -0500

    Write function for find oldest object in array

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
-
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    } return death - birth;
 };
+
+
+
+
+const findTheOldest = function (people) {
+    return people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return oldestAge < currentAge ? current : oldest
+    });
+};
+
+
+
+
+
+
+
+
+//take person object and pull DOB
+//subtract DOD or current date from DOB
+//return person object for highest age
+
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
