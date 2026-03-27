a43f490a34338b813abc9911cda511a6dddac55c
August Johnson <gurojohnson@gmail.com>
2026-03-27 19:08:17 -0400

    Write factorial function for testing suite

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,9 @@
-const factorial = function() {
-
+const factorial = function(x) {
+    if (typeof x !== 'number' || !Number.isInteger(x) || x < 0) {
+        return undefined;
+    } else if (x == 0) {
+        return 1;
+    } return x * factorial(x - 1);
 };
 
 // Do not edit below this line
