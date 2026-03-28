25447d2b274c60401012debc64d9aaa24f38a352
August Johnson <gurojohnson@gmail.com>
2026-03-28 15:29:38 -0400

    Feat/ write function to return pascal triangle row if given row number and test

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,19 @@
-const pascal = function() {
-  
+const pascal = function(n) {
+    if (n < 0) {
+        return;
+    }
+
+    if (n === 1) return [1];
+
+    const prevRow = pascal(n - 1);
+
+    const newRow = [1];
+    for (let i = 1; i < prevRow.length; i++) {
+        newRow.push(prevRow[i - 1] + prevRow[i]);
+    }
+    newRow.push(1);
+
+    return newRow;
 };
   
 // Do not edit below this line
