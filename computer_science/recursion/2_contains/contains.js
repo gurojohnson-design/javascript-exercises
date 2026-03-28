1f5a36e78137b62551ee9ea8b4b6f2ab69f36c50
August Johnson <gurojohnson@gmail.com>
2026-03-27 21:03:58 -0400

    Write function to search nested object tree for given values Test function

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,17 @@
-const contains = function() {
-  
+const contains = function(obj, target) {
+    for (let key in obj) {
+        const val = obj[key];
+
+        // handle NaN specifically since NaN !== NaN
+        if (Number.isNaN(target) && Number.isNaN(val)) return true;
+
+        if (val === target) return true;
+
+        if (typeof val === 'object' && val !== null) {
+            if (contains(val, target)) return true;
+        }
+    }
+    return false;
 };
   
 // Do not edit below this line
