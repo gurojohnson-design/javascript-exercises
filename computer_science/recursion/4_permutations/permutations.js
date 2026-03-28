0e65ee27f9d6c7731c20e7626736b6349c5ee4eb
August Johnson <gurojohnson@gmail.com>
2026-03-28 11:52:20 -0400

    Write function to permute given array and return permutations and test said function

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,21 @@
-const permutations = function() {
-  
-};
-  
+const permutations = function(input) {
+    let result = [];
+
+    function permute(arr, m = []) {
+        if (arr.length === 0) {
+            result.push(m);
+        } else {
+            for (let i = 0; i < arr.length; i++) {
+                let current = arr.slice();
+                let next = current.splice(i, 1);
+                permute(current.slice(), m.concat(next));
+            };
+        }
+    };
+    permute(input);
+    return result;
+}
+
+
 // Do not edit below this line
 module.exports = permutations;
